6fe3d7d63f5830e83afe74d82a4751f20e2de0ae
MalinduAmesh <ameshmalindu006@gmail.com>
2021-04-24 17:21:19 +0530

    v1.1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-document
+
+document.getElementById('Get').addEventListener('click',GetAllData);
+
+function GetAllData(){
+    fetch('https://www.hpb.health.gov.lk/api/get-current-statistical')
+    .then((response) => response.json())
+    .then((data) =>{
+
+
+       document.getElementById('cases-item-lbl1').innerHTML =  `${data.data.local_total_cases}`
+       document.getElementById('cases-item-lbl2').innerHTML = `${data.data.local_new_cases}`
+       document.getElementById('cases-item-lbl3').innerHTML = `${data.data.local_deaths}`
+       document.getElementById('cases-item-lbl4').innerHTML = `${data.data.local_recovered}`
+       console.log(data.data.local_deaths)
+    })
+}
